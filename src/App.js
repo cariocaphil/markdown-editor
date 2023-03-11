de62f247ae9a923aa13a2cf6c07426cd7fcecb47
cariocaphil <cariocaphil@gmail.com>
2023-03-11 23:25:00 +0100

    add basic editor structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
+import React, { useState } from 'react';
+import ReactMarkdown from 'react-markdown';
 import './App.css';
 
 function App() {
+  const [markdown, setMarkdown] = useState('Type your text here');
+
+  function handleChange(e) {
+    setMarkdown(e.target.value);
+  }
   return (
     <div className="app">
-      <textarea />
-
+      <textarea value={markdown} onChange={handleChange} />
+      <ReactMarkdown className="preview" children={markdown}></ReactMarkdown>
       <div className="preview" />
     </div>
   );
